45e6a94a641034196471afdbdecbd5e2d62e3f29
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2021-12-09 11:41:25 +0630

    :bug: optimize the tailwind css

--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [ './src/app/**/*.html',],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
